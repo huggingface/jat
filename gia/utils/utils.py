9c7f7383647fe54a04d285277666a76d11b82153
Edward Beeching <edbeeching@gmail.com>
2022-11-28 12:46:37 +0100

    adds remote method and dict to list conversions

--- gia/utils/utils.py
+++ gia/utils/utils.py
@@ -19,12 +19,29 @@ from gym.spaces import Box, Discrete, Dict
 # }
 # torch_to_numpy_dtype_dict = {value : key for (key, value) in numpy_to_torch_dtype_dict.items()}
 
+
 def to_torch_dtype(numpy_dtype):
     """from_numpy automatically infers type, so we leverage that."""
     x = np.zeros([1], dtype=numpy_dtype)
     t = torch.from_numpy(x)
     return t.dtype
 
-def check_space_is_flat_dict(space : Dict):
-    for k,v in space.items():
+
+def check_space_is_flat_dict(space: Dict):
+    for k, v in space.items():
         assert isinstance(v, (Box, Discrete)), "An instance the thie space {space} is not flat {v}"
+
+
+def _call_remote_method(method, rref, *args, **kwargs):
+    r"""
+    a helper function to call a method on the given RRef
+    """
+    return method(rref.local_value(), *args, **kwargs)
+
+
+def lod_to_dol(lod):
+    return {k: [dic[k] for dic in lod] for k in lod[0]}
+
+
+def dol_to_lod(dol):
+    return [dict(zip(dol, t)) for t in zip(*dol.values())]
