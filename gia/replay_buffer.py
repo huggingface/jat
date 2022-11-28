1d6645900d649c89bdddadc744b7cb36e076a9bf
Edward Beeching <edbeeching@gmail.com>
2022-11-28 12:49:04 +0100

    changes replay buffer to be T,N,x order

--- gia/replay_buffer.py
+++ gia/replay_buffer.py
@@ -3,41 +3,43 @@ from gym.spaces import Dict, Box, Discrete
 from gia.utils.utils import check_space_is_flat_dict, to_torch_dtype
 from gia.utils.tensor_dict import TensorDict
 
-class ReplayBuffer():
+
+class ReplayBuffer:
     def __init__(self, obs_space, action_space, n_agents, rollout_length):
         self.obs_space = obs_space
         self.action_space = action_space
         self.n_agents = n_agents
         self.rollout_length = rollout_length
-        
-        self.buffer = TensorDict(
-            dones=torch.ones(n_agents, rollout_length, dtype=torch.bool),
-            rewards=torch.ones(n_agents, rollout_length, dtype=torch.float32),
-            values=torch.ones(n_agents, rollout_length, dtype=torch.float32)
-        )
 
-        self.buffer["obs"] = self._build_buffer_from_space(obs_space)
-        self.buffer["action"] = self._build_buffer_from_space(action_space)
+        self.buffer = TensorDict(
+            dones=torch.ones(rollout_length, n_agents, dtype=torch.bool),
+            rewards=torch.ones(rollout_length, n_agents, dtype=torch.float32),
+            values=torch.ones(rollout_length, n_agents, dtype=torch.float32),
+        )
+        self.buffer["observations"] = self._build_buffer_from_space(obs_space)
+        self.buffer["actions"] = self._build_buffer_from_space(action_space)
 
     def to(self, device):
         # Moves buffer to device
-        pass 
+        pass
 
     def __getitem__(self, key):
         return self.buffer[key]
 
-    def _build_buffer_from_space(self, space : Dict):
+    def _build_buffer_from_space(self, space: Dict):
         check_space_is_flat_dict(space)
-        sub_buffer = {}
-        for k,v in space.items():
+        sub_buffer = TensorDict()
+        for k, v in space.items():
             if isinstance(v, Box):
-                sub_buffer[k] = torch.ones((self.n_agents, self.rollout_length, *v.shape), dtype=to_torch_dtype(v.dtype))
-                sub_buffer[f"{k}_old_logits"] = torch.ones(self.n_agents, self.rollout_length, *v.shape, dtype=torch.float32)
+                sub_buffer[k] = torch.ones(
+                    (self.rollout_length, self.n_agents, *v.shape), dtype=to_torch_dtype(v.dtype)
+                )
+                # TODO split obs and actions
+                sub_buffer[f"{k}_old_logits"] = torch.ones(
+                    self.rollout_length, self.n_agents, *v.shape, dtype=torch.float32
+                )
             elif isinstance(v, Discrete):
-                sub_buffer[k] = torch.ones((self.n_agents, self.rollout_length, 1), dtype=torch.int32)
-                sub_buffer[f"{k}_old_logits"] = torch.ones(self.n_agents, self.rollout_length, 1,  dtype=torch.float32)
+                sub_buffer[k] = torch.ones((self.rollout_length, self.n_agents, 1), dtype=torch.int32)
+                sub_buffer[f"{k}_old_logits"] = torch.ones(self.rollout_length, self.n_agents, 1, dtype=torch.float32)
 
         return sub_buffer
-
-        
-
--- tests/test_replay_buffer.py
+++ tests/test_replay_buffer.py
@@ -6,52 +6,50 @@ from gym.spaces import Dict, Discrete, Box
 from gia.replay_buffer import ReplayBuffer
 from gia.utils.utils import to_torch_dtype
 
-@pytest.mark.parametrize("obs_space",[Dict(image=Box(0,1,shape=(3,32,40), dtype=np.float32), discrete=Discrete(4))])
-@pytest.mark.parametrize("action_space",[Dict(cont=Box(0,1,shape=(17,), dtype=np.float32), discrete=Discrete(3))])
+
+@pytest.mark.parametrize(
+    "obs_space", [Dict(image=Box(0, 1, shape=(3, 32, 40), dtype=np.float32), discrete=Discrete(4))]
+)
+@pytest.mark.parametrize("action_space", [Dict(cont=Box(0, 1, shape=(17,), dtype=np.float32), discrete=Discrete(3))])
 def test_replay_buffer(obs_space, action_space):
     buffer = ReplayBuffer(obs_space, action_space, 7, 11)
 
-
     subset = buffer[:2]
 
-    assert subset["dones"].shape == (2,11)
+    assert subset["dones"].shape == (2, 7)
     assert subset["dones"].dtype == torch.bool
-    assert subset["rewards"].shape == (2,11)
+    assert subset["rewards"].shape == (2, 7)
     assert subset["rewards"].dtype == torch.float32
-    assert subset["values"].shape == (2,11)
+    assert subset["values"].shape == (2, 7)
     assert subset["values"].dtype == torch.float32
 
-    
-    for k,v in obs_space.items():
+    for k, v in obs_space.items():
         if isinstance(v, Box):
-            assert subset["obs"][k].shape == (2,11, *v.shape)
-            assert subset["obs"][k].dtype == to_torch_dtype(v.dtype)
+            assert subset["observations"][k].shape == (2, 7, *v.shape)
+            assert subset["observations"][k].dtype == to_torch_dtype(v.dtype)
         elif isinstance(v, Discrete):
-            assert subset["obs"][k].shape == (2,11, 1)
-            assert subset["obs"][k].dtype == torch.int32
+            assert subset["observations"][k].shape == (2, 7, 1)
+            assert subset["observations"][k].dtype == torch.int32
         else:
             assert 0, "invalid space in space"
-        
-    for k,v in action_space.items():
+
+    for k, v in action_space.items():
         if isinstance(v, Box):
-            assert subset["action"][k].shape == (2,11, *v.shape)
-            assert subset["action"][k].dtype == to_torch_dtype(v.dtype)
-            assert subset["action"][f"{k}_old_logits"].shape == (2,11, *v.shape)
-            assert subset["action"][f"{k}_old_logits"].dtype == torch.float32
+            assert subset["actions"][k].shape == (2, 7, *v.shape)
+            assert subset["actions"][k].dtype == to_torch_dtype(v.dtype)
+            assert subset["actions"][f"{k}_old_logits"].shape == (2, 7, *v.shape)
+            assert subset["actions"][f"{k}_old_logits"].dtype == torch.float32
         elif isinstance(v, Discrete):
-            assert subset["action"][k].shape == (2,11, 1)
-            assert subset["action"][k].dtype == torch.int32
-            assert subset["action"][f"{k}_old_logits"].shape == (2,11, 1)
-            assert subset["action"][f"{k}_old_logits"].dtype == torch.float32
+            assert subset["actions"][k].shape == (2, 7, 1)
+            assert subset["actions"][k].dtype == torch.int32
+            assert subset["actions"][f"{k}_old_logits"].shape == (2, 7, 1)
+            assert subset["actions"][f"{k}_old_logits"].dtype == torch.float32
         else:
             assert 0, "invalid space in space"
-        
-
-
 
 
 if __name__ == "__main__":
-    obs_space=Dict(image=Box(0,1,shape=(3,32,40), dtype=np.float32), discrete=Discrete(4))
-    action_space=Dict(cont=Box(0,1,shape=(17,), dtype=np.float32), discrete=Discrete(3))
+    obs_space = Dict(image=Box(0, 1, shape=(3, 32, 40), dtype=np.float32), discrete=Discrete(4))
+    action_space = Dict(cont=Box(0, 1, shape=(17,), dtype=np.float32), discrete=Discrete(3))
 
     test_replay_buffer(obs_space, action_space)
