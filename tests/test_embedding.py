454ceca336e4e608a55b0b8440c55adbe123c69e
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2023-02-27 19:13:13 +0100

    fix test embedding

--- tests/test_embedding.py
+++ tests/test_embedding.py
@@ -4,8 +4,9 @@ from gia.model.embedding import Embeddings
 
 
 def test_embeddings():
-    embedding_layer = Embeddings(embedding_dim=32, vocab_size=32, nb_bins=8, max_nb_observation_tokens=7)
-    tokens = torch.randint(0, 1024, (10, 7))
+    nb_bins = 8
+    embedding_layer = Embeddings(embedding_dim=32, vocab_size=32, nb_bins=nb_bins, max_nb_observation_tokens=7)
+    tokens = torch.randint(0, nb_bins, (10, 7))
     tokens[:, 3] = 32 + 8  # separator token
     embeddings = embedding_layer(tokens)
     assert embeddings.shape == (10, 7, 32)
