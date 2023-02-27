a3299466eafce5de93a697866fab502c38759754
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2023-02-27 19:04:54 +0100

    update test_tokenizer

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -4,11 +4,12 @@ from gia.tokenizers import Tokenizer
 
 
 def test_continuous_tokenizer():
-    continous_tokenizer = Tokenizer()
-    observations = torch.rand(10, 9) * 20 - 10  # Random observations in [-10, 10]
+    continous_tokenizer = Tokenizer(nb_bins=10, token_shift=256)
+    seprator_token = 10 + 256
+    tensors = torch.rand(10, 9) * 20 - 10  # Random tensors in [-10, 10]
     actions = torch.rand(10, 3) * 20 - 10  # Random actions in [-10, 10]
-    tokens = continous_tokenizer(observations, actions)
+    tokens = continous_tokenizer(tensors=tensors, actions=actions)
     assert tokens.shape == (10, 13)  # 9 observations, 1 separator, 3 actions
-    assert torch.all(tokens[:, :9] != continous_tokenizer.nb_bins)  # Observation tokens are not separator tokens
-    assert torch.all(tokens[:, 9] == continous_tokenizer.nb_bins)  # Separator token
-    assert torch.all(tokens[:, 10:] != continous_tokenizer.nb_bins)  # Action tokens are not separator tokens
+    assert torch.all(tokens[:, :9] != seprator_token)  # Observation tokens are not separator tokens
+    assert torch.all(tokens[:, 9] == seprator_token)  # Separator token
+    assert torch.all(tokens[:, 10:] != seprator_token)  # Action tokens are not separator tokens
