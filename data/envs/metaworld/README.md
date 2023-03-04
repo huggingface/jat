1aed0a3551e408581a6bd4a6a722fd2818a4dd6b
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-03-04 16:20:03 +0100

    train and enjoy scripts

--- data/envs/metaworld/README.md
+++ data/envs/metaworld/README.md
@@ -1,22 +1,21 @@
-## TODO
+# Metaworld dataset
 
-- [ ] Improve train and enjoy to be generic to every env
-- [ ] Create setup.py with the dependencies (use the branch O.26 of Metaworld)
-- [ ] Solve warnings
-
-
-Command lines:
-
-Train:
+## Installation
 
 ```sh
-python train.py --env pick-place-v2 --with_wandb=True --wandb_user=qgallouedec --wandb_project sample_facotry_metaworld
+pip install -r requirements.txt
+```
+
+## Train
+
+```sh
+./train_all.sh
 ```
 
 Push to hub:
 
 ```sh
-python enjoy.py --algo=APPO --env=pick-place-v2 --experiment=default_experiment --train_dir=./train_dir --max_num_episodes=10 --push_to_hub --hf_repository=qgallouedec/pick-place-v2-sf --save_video --no_render --enjoy_script=enjoy --train_script=train --load_checkpoint_kind best
+python enjoy.py --algo=APPO --env=pick-place-v2 --experiment=pick-place-v2 --train_dir=./train_dir --max_num_episodes=10 --push_to_hub --hf_repository=qgallouedec/pick-place-v2-sf --save_video --no_render --enjoy_script=enjoy --train_script=train --load_checkpoint_kind best
 ```
 
 Generate dataset:
--- data/envs/metaworld/enjoy.py
+++ data/envs/metaworld/enjoy.py
@@ -1,31 +1,77 @@
 import sys
-from typing import Optional
-from sample_factory.enjoy import enjoy
+from typing import Optional, Dict
 
-import metaworld
 import gym
-
+import metaworld
 from sample_factory.cfg.arguments import parse_full_cfg, parse_sf_args
+from sample_factory.enjoy import enjoy
 from sample_factory.envs.env_utils import register_env
-from sample_factory.train import run_rl
 
-def parse_args(argv=None, evaluation=False):
-    # parse the command line arguments to build
-    parser, partial_cfg = parse_sf_args(argv=argv, evaluation=evaluation)
-#    add_custom_env_args(partial_cfg.env, parser, evaluation=evaluation)
-#    custom_env_override_defaults(partial_cfg.env, parser)
-    final_cfg = parse_full_cfg(parser, argv)
-    return final_cfg
+ENV_NAMES = [
+    "assembly-v2",
+    "basketball-v2",
+    "bin-picking-v2",
+    "box-close-v2",
+    "button-press-topdown-v2",
+    "button-press-topdown-wall-v2",
+    "button-press-v2",
+    "button-press-wall-v2",
+    "coffee-button-v2",
+    "coffee-pull-v2",
+    "coffee-push-v2",
+    "dial-turn-v2",
+    "disassemble-v2",
+    "door-close-v2",
+    "door-lock-v2",
+    "door-open-v2",
+    "door-unlock-v2",
+    "drawer-close-v2",
+    "drawer-open-v2",
+    "faucet-close-v2",
+    "faucet-open-v2",
+    "hammer-v2",
+    "hand-insert-v2",
+    "handle-press-side-v2",
+    "handle-press-v2",
+    "handle-pull-side-v2",
+    "handle-pull-v2",
+    "lever-pull-v2",
+    "peg-insert-side-v2",
+    "peg-unplug-side-v2",
+    "pick-out-of-hole-v2",
+    "pick-place-v2",
+    "pick-place-wall-v2",
+    "plate-slide-back-side-v2",
+    "plate-slide-back-v2",
+    "plate-slide-side-v2",
+    "plate-slide-v2",
+    "push-back-v2",
+    "push-v2",
+    "push-wall-v2",
+    "reach-v2",
+    "reach-wall-v2",
+    "shelf-place-v2",
+    "soccer-v2",
+    "stick-pull-v2",
+    "stick-push-v2",
+    "sweep-into-v2",
+    "sweep-v2",
+    "window-close-v2",
+    "window-open-v2",
+]
 
 
-def make_custom_env(full_env_name: str, cfg=None, env_config=None, render_mode: Optional[str] = None):
-    # see the section below explaining arguments
-    return gym.make("pick-place-v2")
+def make_custom_env(
+    full_env_name: str, cfg: Optional[Dict] = None, env_config: Optional[Dict] = None, render_mode: Optional[str] = None
+) -> gym.Env:
+    return gym.make(full_env_name, render_mode=render_mode)
 
-def main():
-    """Script entry point."""
-    register_env("pick-place-v2", make_custom_env)
-    cfg = parse_args(evaluation=True)
+
+def main() -> int:
+    for env_name in ENV_NAMES:
+        register_env(env_name, make_custom_env)
+    parser, _ = parse_sf_args(argv=None, evaluation=True)
+    cfg = parse_full_cfg(parser)
     status = enjoy(cfg)
     return status
 
