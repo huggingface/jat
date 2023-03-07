2d662f3400cb700e72d609fe34f1ec3d16529e94
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-03-07 17:23:40 +0100

    new train defaults

--- data/envs/metaworld/train.py
+++ data/envs/metaworld/train.py
@@ -1,5 +1,6 @@
+import argparse
 import sys
-from typing import Optional, Dict
+from typing import Dict, Optional
 
 import gym
 import metaworld
@@ -62,15 +63,61 @@ ENV_NAMES = [
 
 
 def make_custom_env(
-    full_env_name: str, cfg: Optional[Dict] = None, env_config: Optional[Dict] = None, render_mode: Optional[str] = None
+    full_env_name: str,
+    cfg: Optional[Dict] = None,
+    env_config: Optional[Dict] = None,
+    render_mode: Optional[str] = None,
 ) -> gym.Env:
     return gym.make(full_env_name, render_mode=render_mode)
+
+
+def override_defaults(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
+    parser.set_defaults(
+        batched_sampling=False,
+        device="cpu",
+        num_workers=8,
+        num_envs_per_worker=8,
+        worker_num_splits=2,
+        train_for_env_steps=100_000_000,
+        encoder_mlp_layers=[64, 64],
+        env_frameskip=1,
+        nonlinearity="tanh",
+        batch_size=1024,
+        kl_loss_coeff=0.1,
+        use_rnn=False,
+        adaptive_stddev=False,
+        policy_initialization="torch_default",
+        reward_scale=0.01,
+        rollout=64,
+        max_grad_norm=3.5,
+        num_epochs=2,
+        num_batches_per_epoch=4,
+        ppo_clip_ratio=0.2,
+        value_loss_coeff=1.3,
+        exploration_loss_coeff=0.0,
+        learning_rate=0.00295,
+        lr_schedule="linear_decay",
+        shuffle_minibatches=False,
+        gamma=0.99,
+        gae_lambda=0.95,
+        with_vtrace=False,
+        recurrence=1,
+        normalize_input=True,
+        normalize_returns=True,
+        value_bootstrap=True,
+        experiment_summaries_interval=3,
+        save_every_sec=15,
+        serial_mode=False,
+        async_rl=False,
+    )
+    return parser
 
 
 def main() -> int:
     for env_name in ENV_NAMES:
         register_env(env_name, make_custom_env)
     parser, _ = parse_sf_args(argv=None, evaluation=False)
+    parser = override_defaults(parser)
     cfg = parse_full_cfg(parser)
     status = run_rl(cfg)
     return status
