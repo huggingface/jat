3861bb235f81ed4f6a2e4551f9e24cdb4bbc0538
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-03-20 11:33:22 +0100

    refactor

--- gia/tokenizers/multimodal_tokenizer.py
+++ gia/tokenizers/multimodal_tokenizer.py
@@ -4,7 +4,7 @@ import torch
 from torch import Tensor, nn
 from transformers import AutoTokenizer
 
-from gia.tokenizers.continuous_tokenizer import ContinuousTokenizer
+from gia.utils.utils import discretize, inverse_mu_law, mu_law
 
 
 def is_text(x: Any) -> bool:
@@ -78,6 +78,17 @@ def concat_multi_dim_iterables(l: List[List[Iterable]]) -> List[List]:
     return output
 
 
+def transform_func(examples):
+    """Convert image to uint8.
+
+    Fix of https://github.com/huggingface/datasets/issues/4623
+    """
+    for key in examples.keys():
+        if isinstance(examples, Tensor) and examples[key].dim() == 4:
+            examples[key] = examples[key].to(dtype=torch.uint8)
+    return examples
+
+
 class DiscreteTokenizer(nn.Module):
     """
     Discrete tokenizer.
@@ -124,15 +135,140 @@ class BooleanTokenizer(DiscreteTokenizer):
         return super().forward(x)
 
 
-def transform_func(examples):
-    """Convert image to uint8.
-
-    Fix of https://github.com/huggingface/datasets/issues/4623
+class ContinuousTokenizer(nn.Module):
     """
-    for key in examples.keys():
-        if isinstance(examples, Tensor) and examples[key].dim() == 4:
-            examples[key] = examples[key].to(dtype=torch.uint8)
-    return examples
+    Continous tokenizer.
+
+    First, each floating point element of input tensor is mu-law companded as in WaveNet (Oord et al., 2016).
+    Then, the tensor is discretized into integers in the range [0, nb_bins-1].
+
+    Example:
+        >>> import torch
+        >>> tokenizer = ContinuousTokenizer()
+        >>> tensors = torch.tensor([[-0.6285, -5.1349,  3.9317, -3.5854],
+        ...                         [ 3.9263, -1.8975,  7.3103, -7.3467],
+        ...                         [-6.9724, -2.9982,  9.1740,  0.1002]])
+        >>> tokenizer(tensors)
+        tensor([[302, 197, 813, 215],
+                [813, 247, 844, 179],
+                [181, 224, 856, 633]])
+
+    Args:
+        mu_law_compand (bool, optional): Whether to use mu-law companding. Defaults to True.
+        mu (float, optional): μ parameter. Defaults to 100.
+        M (float, optional): M parameter. Defaults to 256.
+        nb_bins (int, optional): Number of bins for the discretization. Defaults to 1024.
+    """
+
+    def __init__(
+        self, mu_law_compand: bool = True, mu: float = 100, M: float = 256, nb_bins: int = 1024, shift: int = 0
+    ) -> None:
+        super().__init__()
+        self.mu_law_compand = mu_law_compand
+        self.mu = mu
+        self.M = M
+        self.nb_bins = nb_bins
+        self.shift = shift
+
+    def forward(self, x: Tensor) -> Dict[str, Tensor]:
+        # Normalize tensors to the range [-1, 1]
+        if self.mu_law_compand:
+            x = mu_law(x, mu=self.mu, M=self.M)
+
+        # Clip to the range [-1, 1]
+        x = torch.clamp(x, -1.0, 1.0)
+
+        # Discretize tensors
+        x = discretize(x, nb_bins=self.nb_bins)
+
+        # Unsqueeze when the input is a vector
+        if x.dim() == 1:
+            x = x.unsqueeze(1)
+
+        # Unsqueeze if needed
+        tokens = x + self.shift
+        return {
+            "tokens": tokens.tolist(),
+            "attention_mask": torch.ones_like(tokens).tolist(),
+        }
+
+    def inverse_tokenize_continuous(self, tokens: Tensor) -> Tensor:
+        """
+        Inverse tokenize continous.
+
+        First, each integer element of input tensor is mapped to the center of the corresponding bin.
+        Then, the tensor is de-mu-law companded if needed.
+
+        Args:
+            tokens (Tensor): Tokens
+
+        Returns:
+            Tensor: Reconstructed tensor
+        """
+
+        # Maps tokens from [0, nb_bins-1] to [-1, 1]
+        # We map the bin number to the center of the bin
+        x = (2 * tokens + 1) / self.nb_bins - 1
+
+        # De-mu-law compand tensors
+        if self.mu_law_compand:
+            x = inverse_mu_law(x, mu=self.mu, M=self.M)
+
+        return x
+
+
+class PatchExtractor(nn.Module):
+    """
+    Extract patches from images.
+
+    Args:
+        patch_size (int, optional): Patch size. Defaults to 16.
+
+    Returns:
+        dict: A dictionary containing the following keys and values:
+            - "patches": A tensor of shape (batch_size, H*W, channels, patch_size, patch_size).
+            - "row_indices": A tensor of shape (batch_size, H*W) containing row indices of patches.
+            - "col_indices": A tensor of shape (batch_size, H*W) containing column indices of patches.
+            - "attention_mask": A tensor of shape (batch_size, H*W) containing attention mask.
+
+    Examples:
+        >>> x = torch.randn(2, 3, 16, 12)
+        >>> patch_extractor = PatchExtractor(patch_size=4)
+        >>> result = patch_extractor(x)
+        >>> result["patches"].shape
+        torch.Size([2, 12, 3, 4, 4])
+        >>> result["row_indices"]
+        [[0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2], [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2]]
+        >>> result["col_indices"]
+        [[0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3], [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3]]
+        >>> result["attention_mask"]
+        [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+    """
+
+    def __init__(self, patch_size: int = 16) -> None:
+        super().__init__()
+        self.patch_size = patch_size
+
+    def forward(self, x: Tensor) -> Tensor:
+        _, _, height, width = x.shape
+        assert height % self.patch_size == 0, "Height must be divisible by patch_size"
+        assert width % self.patch_size == 0, "Width must be divisible by patch_size"
+        patches = x.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size)
+        # Get H and W
+        batch_size, H, W = patches.shape[:3]
+        # Get the row and the column indices. First dim is still the batch size
+        rows, cols = torch.meshgrid(torch.arange(H), torch.arange(W), indexing="ij")
+        row_indices = rows.flatten().unsqueeze(0).repeat(batch_size, 1)
+        col_indices = cols.flatten().unsqueeze(0).repeat(batch_size, 1)
+        # Flatten the patches (from shape (batch_size, H, W, channels, patch_size, patch_size)
+        # to (batch_size, H*W, channels, patch_size, patch_size))
+        patches = patches.flatten(1, 2)
+        return {
+            "patches": patches,
+            "row_indices": row_indices.tolist(),
+            "col_indices": col_indices.tolist(),
+            "attention_mask": torch.ones(batch_size, H * W, dtype=torch.int).tolist(),
+        }
 
 
 class MultiModalTokenizer(nn.Module):
@@ -161,6 +297,7 @@ class MultiModalTokenizer(nn.Module):
         M (float, optional): Mu-law companding parameter. Defaults to 256.
         nb_bins (int, optional): Number of bins for the discretization. Defaults to 1024.
         token_shift (int, optional): Shift for the discrete tokens. Defaults to 32_000.
+        patch_size (int, optional): Patch size. Defaults to 16.
         use_separator (bool, optional): Whether to use a separator token between observations and actions. Defaults to True.
     """
 
@@ -170,15 +307,16 @@ class MultiModalTokenizer(nn.Module):
         M: float = 256,
         nb_bins: int = 1024,
         token_shift: int = 32_000,
+        patch_size: int = 16,
         use_separator: bool = True,
-    ):
+    ) -> None:
         super().__init__()
         self.token_shift = token_shift
         self.use_separator = use_separator
         # Token for separating observations and actions
         self.separator_token = torch.tensor(nb_bins + token_shift, dtype=torch.int64)
         self.text_tokenizer = AutoTokenizer.from_pretrained("albert-base-v2")
-        self.image_tokenizer = ...  # TODO:
+        self.patch_extractor = PatchExtractor(patch_size)
         self.dicrete_tokenizer = DiscreteTokenizer(token_shift)
         self.boolean_tokenizer = BooleanTokenizer(token_shift)
         self.continuous_tokenizer = ContinuousTokenizer(mu=mu, M=M, nb_bins=nb_bins, shift=token_shift)
@@ -192,9 +330,8 @@ class MultiModalTokenizer(nn.Module):
                 tokens = self.text_tokenizer(value)
                 # Rename input_ids to tokens
                 tokens["tokens"] = tokens.pop("input_ids")
-            elif is_image(value):
-                continue
-                tokens = self.image_tokenizer(value)
+            elif is_image(value):  # Warning: special case for images: output is a patch, not a number
+                tokens = self.patch_extractor(value)
             elif is_discrete(value):
                 tokens = self.dicrete_tokenizer(value)
             elif is_continuous(value):
@@ -208,69 +345,10 @@ class MultiModalTokenizer(nn.Module):
                 output[f"{key}_{_key}"] = _value
         return output
 
-        # Get the observation keys
-        observation_keys = sorted([key for key in inputs.keys() if key not in ["actions", "dones", "rewards"]])
 
-        tokens = []
-
-        # Observation part
-        # Detect the inputs corresponding to texts
-        text_keys = [key for key in observation_keys if is_text(inputs[key])]
-        for key in sorted(text_keys):
-            texts = inputs[key]
-            text_tokens = self.text_tokenizer(texts)
-            tokens.append(text_tokens)
-            # Here, tokens is a list dict, whose keys are tokens, attention_mask
-            # tokens is a list of list of int
-            # attention_mask is a list of list of int (1 for real tokens, 0 for padding)
-
-        # Detect the inputs corresponding to images
-        image_keys = [key for key in observation_keys if is_image(inputs[key])]
-        for key in sorted(image_keys):
-            images = inputs[key]
-            continue
-            raise NotImplementedError("Images tokenisation is not implemented yet.")
-            image_tokens = self.image_tokenizer(images)
-            tokens.append(image_tokens)
-
-        # Detect the inputs corresponding to dicrete tensors
-        discrete_tensor_keys = [key for key in observation_keys if is_discrete(inputs[key]) and key != "actions"]
-        for key in sorted(discrete_tensor_keys):
-            tensors = inputs[key]
-            tensor_tokens = self.dicrete_tokenizer(tensors)
-            tokens.append(tensor_tokens)
-
-        # Detect the inputs corresponding to continuous tensors
-        continuous_tensor_keys = [key for key in observation_keys if is_continuous(inputs[key]) and key != "actions"]
-        for key in sorted(continuous_tensor_keys):
-            tensors = inputs[key]
-            tensor_tokens = self.continuous_tokenizer(tensors)
-            tokens.append(tensor_tokens)
-
-        # Add the separator token
-        if self.use_separator:
-            sequence_length = len(tokens[0]["tokens"])
-            separator_tokens = self.separator_token.repeat(sequence_length, 1)
-            tokens.append(
-                {
-                    "tokens": separator_tokens.tolist(),
-                    "attention_mask": torch.ones_like(separator_tokens).tolist(),
-                }
-            )
-
-        # Action part
-        if "actions" in inputs:
-            actions = inputs["actions"]
-            # Detect the inputs corresponding to dicrete actions
-            if actions.dtype == torch.int64:  # If actions are discrete, the tokens are simply the actions
-                tokens.append(self.dicrete_tokenizer(actions))
-            elif actions.dtype == torch.float32:
-                tokens.append(self.continuous_tokenizer(actions))
-            else:
-                raise ValueError(f"Invalid actions dtype: {actions.dtype}, expected torch.int64 or torch.float32.")
-
-        # Concatenate the tokens
-        tokens = concat_multi_dim_iterables([token["tokens"] for token in tokens])
-        attention_mask = concat_multi_dim_iterables([token["attention_mask"] for token in tokens])
-        loss_mask = concat_multi_dim_iterables([token["loss_mask"] for token in tokens])
-        return {"tokens": tokens, "attention_mask": attention_mask, "loss_mask": loss_mask}
+if __name__ == "__main__":
+    image = torch.rand(2, 3, 16, 12)
+    text = ["Go right", "Go left"]
+    multimodal_tokenizer = MultiModalTokenizer(patch_size=4)
+    encoding = multimodal_tokenizer({"images": image, "texts": text})
+    print(encoding)
