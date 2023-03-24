5db8d85fb7eb08cbec81854626582336e37b479f
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-03-24 09:46:11 +0100

    remove mujocodataset

--- gia/datasets/__init__.py
+++ gia/datasets/__init__.py
@@ -1,4 +1,3 @@
 from .gia_dataset import load_gia_dataset
-from .mujoco_dataset import MujocoDataset, MujocoTaskDataset
 
-__all__ = ["load_gia_dataset", "MujocoDataset", "MujocoTaskDataset"]
+__all__ = ["load_gia_dataset"]
