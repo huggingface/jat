ff43324074679d106c1554377b2df9f2576fd96c
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-04-07 21:46:22 +0200

    arguments

--- gia/config/__init__.py
+++ gia/config/__init__.py
@@ -1,4 +1,3 @@
-from .arguments import Arguments
-from .config import Config
+from .arguments import Arguments, DatasetArguments, ModelArguments, TrainingArguments
 
-__all__ = ["Arguments", "Config"]
+__all__ = ["Arguments", "DatasetArguments", "ModelArguments", "TrainingArguments"]
--- gia/config/arguments.py
+++ gia/config/arguments.py
@@ -5,57 +5,53 @@ from typing import List, Optional, Union
 @dataclass
 class TrainingArguments:
     """
-    Configuration for training model.
+    Arguments related to training and evaluation.
     """
 
-    model_ckpt: Optional[str] = field(default="", metadata={"help": "Model name or path of model to be trained."})
-    save_dir: Optional[str] = field(
+    model_ckpt: str = field(default="", metadata={"help": "Model name or path of model to be trained."})
+    save_dir: str = field(
         default="./runs/run01",
         metadata={"help": "Save dir where model repo is cloned and models updates are saved to."},
     )
-    dataset_name_train: Optional[str] = field(default="", metadata={"help": "Name or path of training dataset."})
-    dataset_name_valid: Optional[str] = field(default="", metadata={"help": "Name or path of validation dataset."})
-    train_batch_size: Optional[int] = field(default=8, metadata={"help": "Batch size for training."})
-    valid_batch_size: Optional[int] = field(default=2, metadata={"help": "Batch size for evaluation."})
-    weight_decay: Optional[float] = field(default=0.1, metadata={"help": "Value of weight decay."})
-    shuffle_buffer: Optional[int] = field(
-        default=10000, metadata={"help": "Size of buffer used to shuffle streaming dataset."}
+    dataset_name_train: str = field(default="", metadata={"help": "Name or path of training dataset."})
+    dataset_name_valid: str = field(default="", metadata={"help": "Name or path of validation dataset."})
+    train_batch_size: int = field(default=8, metadata={"help": "Batch size for training."})
+    valid_batch_size: int = field(default=2, metadata={"help": "Batch size for evaluation."})
+    weight_decay: float = field(default=0.1, metadata={"help": "Value of weight decay."})
+    learning_rate: float = field(default=2e-4, metadata={"help": "Learning rate fo training."})
+    lr_scheduler_type: str = field(default="cosine", metadata={"help": "Learning rate scheduler type."})
+    num_warmup_steps: int = field(
+        default=750,
+        metadata={
+            "help": "The number of warmup steps to do. This is not required by all schedulers (hence the argument "
+            "being  optional), the function will raise an error if it's unset and the scheduler type requires it."
+        },
     )
-    learning_rate: Optional[float] = field(default=2e-4, metadata={"help": "Learning rate fo training."})
-    lr_scheduler_type: Optional[str] = field(default="cosine", metadata={"help": "Learning rate."})
-    num_warmup_steps: Optional[int] = field(
-        default=750, metadata={"help": "Number of warmup steps in the learning rate schedule."}
-    )
-    gradient_accumulation_steps: Optional[int] = field(
-        default=16, metadata={"help": "Number of gradient accumulation steps."}
-    )
-    gradient_checkpointing: Optional[bool] = field(
+    gradient_accumulation_steps: int = field(default=16, metadata={"help": "Number of gradient accumulation steps."})
+    gradient_checkpointing: bool = field(
         default=False, metadata={"help": "Use gradient checkpointing to reduce memory footprint."}
     )
-    max_train_steps: Optional[int] = field(default=50000, metadata={"help": "Maximum number of training steps."})
-    max_eval_steps: Optional[int] = field(
+    max_train_steps: int = field(default=50000, metadata={"help": "Maximum number of training steps."})
+    max_eval_steps: int = field(
         default=-1, metadata={"help": "Maximum number of evaluation steps. If -1 the full dataset is evaluated."}
     )
-    seq_length: Optional[int] = field(default=1024, metadata={"help": "Sequence lengths used for training."})
-    seed: Optional[int] = field(default=1, metadata={"help": "Training seed."})
-    save_checkpoint_steps: Optional[int] = field(
+    seed: int = field(default=1, metadata={"help": "Training seed."})
+    save_checkpoint_steps: int = field(
         default=1024,
         metadata={"help": "Interval to save checkpoints. Measured as number of forward passes not training steps."},
     )
-    resume_from_checkpoint: Optional[str] = field(
+    resume_from_checkpoint: str = field(
         default=None, metadata={"help": "States path if the training should continue from a checkpoint folder."}
     )
-    tokenized: Optional[bool] = field(default=False, metadata={"help": "If True the data is pretokenized."})
 
 
 @dataclass
 class ModelArguments:
+    """
+    Arguments related to the model.
+    """
+
     model_name: str = field(default="EleutherAI/gpt-neo-125M", metadata={"help": "The name of the model"})
-    vocab_size: Optional[int] = field(
-        default=32_000 + 1024 + 1,
-        metadata={"help": "The size of the model vocabulary, including the tokenization of observations"},
-    )
-    seq_len: int = field(default=1024, metadata={"help": "The length (number of tokens) of a sequence."})
     max_position_embeddings: Optional[int] = field(
         default=32, metadata={"help": "The maximum number of position embeddings."}
     )
@@ -93,7 +89,13 @@ class ModelArguments:
 
 @dataclass
 class DatasetArguments:
-    task_name: str = field(default="mujoco-ant", metadata={"help": "The name of the task to load."})
+    task_names: Union[str, List[str]] = field(
+        default="all",
+        metadata={
+            "help": "Name or list of names of the tasks to load. See the available tasks in "
+            "https://huggingface.co/datasets/gia-project/gia-dataset. If 'all', load all the tasks. Defaults to 'all'."
+        },
+    )
     seq_len: int = field(default=1024, metadata={"help": "The length (number of tokens) of a sequence."})
     p_prompt: float = field(
         default=0.25, metadata={"help": "The probability of including a prompt at the beginning of a sequence."}
@@ -122,6 +124,9 @@ class DatasetArguments:
     load_from_cache: Optional[bool] = field(
         default=True, metadata={"help": "Whether to load the dataset from the cache files."}
     )
+    shuffle: bool = field(default=True, metadata={"help": "Whether to shuffle the dataset. Defaults to True."})
+    # Is batch_size confusing when we have train_batch_size and valid_batch_size?
+    batch_size: int = field(default=8, metadata={"help": "The batch size."})
 
 
 @dataclass
