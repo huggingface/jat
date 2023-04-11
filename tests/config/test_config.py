19f5f829679977965ee4d406a9262e651ce8db44
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-04-11 16:51:03 +0200

    rm arg.yml and test_config

--- tests/config/test_config.py
+++
@@ -1,9 +0,0 @@
-from hydra import compose, initialize
-
-
-def test_load_config():
-    with initialize(version_base=None, config_path="../../gia/config"):
-        config = compose(
-            config_name="config",
-        )
-        print(config)
