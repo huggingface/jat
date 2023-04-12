b4327b78346ab73c6a058c75726f6fe04b70a2a3
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-04-12 12:15:28 +0200

    fix example

--- examples/mixed_loader.py
+++ examples/mixed_loader.py
@@ -1,5 +1,8 @@
 from gia.datasets import get_dataloader
-dataloader = get_dataloader(["babyai-go-to", "mujoco-ant"], shuffle=True, batch_size=2)
+from gia.config import DatasetArguments
+
+args = DatasetArguments(task_names=["babyai-go-to", "mujoco-ant"], shuffle=True, batch_size=2)
+dataloader = get_dataloader(args)
 iterator = iter(dataloader)
 
 for _ in range(4):
