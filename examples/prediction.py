43e22ede399ed1540a1cf1bcc88176e252610546
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-04-19 12:26:20 +0200

    update prediction example

--- examples/prediction.py
+++ examples/prediction.py
@@ -3,25 +3,17 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 from gia.config import Arguments
-from gia.datasets import load_mixed_dataset
+from gia.datasets import collate_fn, load_mixed_dataset
 from gia.model import GiaModel
-
-
-def collate(batch):
-    for sample in batch:
-        for key in sample.keys():
-            sample[key] = torch.from_numpy(sample[key])
-    return batch
-
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 args = Arguments(task_names=["babyai-go-to", "mujoco-ant"], batch_size=1)
 dataset = load_mixed_dataset(args)
-dataloader = DataLoader(dataset, shuffle=True, collate_fn=collate)
+dataloader = DataLoader(dataset, shuffle=True, collate_fn=collate_fn)
 model = GiaModel(args).to(device)
 for batch in tqdm(dataloader):
-    for sample in batch:
+    for sample in batch: # This loop can be removed when you use accelerate
         for key in sample.keys():
             sample[key] = sample[key].to(device)
     out = model(batch)
