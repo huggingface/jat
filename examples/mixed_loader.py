c7835f7e0e79be5cefcb258e6cda5591995db66e
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-04-20 10:56:57 +0200

    rm mix_loader example

--- examples/mixed_loader.py
+++
@@ -1,9 +0,0 @@
-from gia.datasets import get_dataloader
-from gia.config import DatasetArguments
-
-args = DatasetArguments(task_names=["babyai-go-to", "mujoco-ant"], shuffle=True, batch_size=2)
-dataloader = get_dataloader(args)
-iterator = iter(dataloader)
-
-for _ in range(4):
-    print(next(iterator).keys())
