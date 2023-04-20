b8976ed22724c0d62da7ef56b9db917af44ea878
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-04-20 20:53:13 +0200

    update example

--- examples/load_text_only_dataset.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from datasets import load_dataset
-
-dataset = load_dataset("ClementRomac/cleaned_deduplicated_oscar")
-observations = np.array([dataset["train"][idx]["text"] for idx in range(len(dataset["train"]))])
-dones = np.array([True for _ in range(30)])
-dataset = {"text_observations": observations, "dones":dones}
