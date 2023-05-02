d1d18b3f8191d334e3538aecf26700c244bcda5c
edbeeching <edbeeching@gmail.com>
2023-05-02 21:32:57 +0200

    puts unused columns back

--- examples/train.py
+++ examples/train.py
@@ -1,4 +1,3 @@
-
 from gia.datasets import collate_fn, load_mixed_dataset
 from gia.config import parse_args
 
@@ -7,13 +6,13 @@ from transformers import Trainer
 
 args = parse_args()
 args.task_names = "mujoco"
-
+args.remove_unused_columns = False
 dataset = load_mixed_dataset(args)
 
 model = GiaModel(args)
 
 trainer = Trainer(
-    model, 
+    model,
     args,
     data_collator=collate_fn,
     train_dataset=dataset,
