bb4fe741bafda9cdce30e0958357e1f2335b04fc
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-05-08 22:50:09 +0200

    Update load_dataset example

--- examples/load_dataset.py
+++ examples/load_dataset.py
@@ -1,3 +1,13 @@
-from gia.datasets import load_gia_dataset
+from gia.datasets.core import load_gia_dataset
+import numpy as np
 
-dataset = load_gia_dataset(["mujoco-ant", "mujoco-hopper"])
+dataset = load_gia_dataset("mujoco-ant")
+print(
+    f"""Keys: {list(dataset.keys())}
+Number of episodes: {len(dataset)}
+Length of the first episode: {len(dataset[0]['continuous_observations'])}
+First observation: {np.round(dataset[0]['continuous_observations'][0], 1)}
+First action: {np.round(dataset[0]['continuous_actions'][0], 1)}
+First reward: {np.round(dataset[0]['rewards'][0], 1)}
+"""
+)
