b8baeccdf4cf3ac0b257a70c4f4206f910a7e700
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-05-09 09:05:27 +0200

    example embed (not working)

--- examples/embed.py
+++ examples/embed.py
@@ -1,14 +1,17 @@
 from torch.utils.data import DataLoader
 
 from gia.config import Arguments
-from gia.datasets import collate_fn, load_mixed_dataset
-from gia.model.embedding import Embeddings
+from gia.datasets import collate_fn, load_gia_dataset
+from gia.datasets.utils import DatasetDict
+from gia.processing import GiaProcessor
 
-args = Arguments(task_names=["babyai-go-to"], embed_dim=128)
-dataset = load_mixed_dataset(args)
+args = Arguments(task_names=["mujoco-ant"], embed_dim=128)
+dataset = load_gia_dataset(args.task_names)
+
+processor = GiaProcessor(args)
+dataset = DatasetDict(processor(**dataset))
+
 dataloader = DataLoader(dataset, batch_size=args.batch_size, collate_fn=collate_fn)
-embeddings = Embeddings(args)
 batch = next(iter(dataloader))
-embeds = [embeddings(sample) for sample in batch]
-print(len(embeds))  # 8 (= batch_size)
-print(embeds[0].keys())  # dict_keys(['embeddings', 'loss_mask', 'attention_mask', 'tokens'])
+
+print(batch)
