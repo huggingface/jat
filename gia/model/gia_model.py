0d923e0e250e7d6a61b4eb608ad0c9012ac46779
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2023-05-10 18:44:20 +0200

    update gia-model

--- gia/model/gia_model.py
+++ gia/model/gia_model.py
@@ -1,57 +1,40 @@
-from typing import List
-
-import torch
-from torch import Tensor, nn
+from torch import nn
 from transformers import AutoConfig, AutoModelForCausalLM
 from transformers.modeling_outputs import CausalLMOutputWithPast
-
 from gia.config import ModelArguments
 from gia.model.embedding import Embeddings
-
-
-def pad_and_cat(tensor_list: List[Tensor], max_len: int) -> Tensor:
-    """
-    Pad right with zeros, in the 2nd dimension and concatenate.
-
-    Args:
-        tensor_list (List[Tensor]): List of tensors to pad. Shapes must be (N, X, ...) where X can vary.
-        max_len (int): The output tensor will have the shape (N, max_len, ...).
-
-    Returns:
-        Tensor: The padded and concatenated tensor.
-
-    Example:
-        >>> x = [torch.rand(1, 10, 3), torch.rand(1, 5, 3), torch.rand(1, 15, 3)]
-        >>> pad_and_cat(x, 20).shape
-        torch.Size([3, 20, 3])
-    """
-    output_shape = [len(tensor_list), max_len, *tensor_list[0].shape[2:]]
-    output = torch.zeros(output_shape, dtype=tensor_list[0].dtype, device=tensor_list[0].device)
-    for i, t in enumerate(tensor_list):
-        output[i, : t.shape[1]] = t
-    return output
 
 
 class GiaModel(nn.Module):
     def __init__(self, args: ModelArguments) -> None:
         super().__init__()
+        if args.use_separator:
+            vocab_size = args.text_vocab_size + args.nb_bins + 1
+        else:
+            vocab_size = args.text_vocab_size + args.nb_bins
         if args.use_pretrained:
             raise NotImplementedError("Pretrained models are not supported yet.")
             # self.model = AutoModelForCausalLM.from_pretrained(args.model_name)
         else:
             config = AutoConfig.from_pretrained(args.model_name)
-            config.vocab_size = args.text_vocab_size + args.nb_bins
-            if args.use_separator:
-                config.vocab_size += 1
+            config.vocab_size = vocab_size
             config.max_position_embeddings = args.seq_len  # this is a workaround for gpt-neo's local self attn
             config.hidden_size = args.embed_dim
             self.model = AutoModelForCausalLM.from_config(config)
 
-        self.emb = Embeddings(args)
+        self.emb = Embeddings(
+            args.embed_dim,
+            vocab_size,
+            args.max_nb_observation_tokens,
+            args.patch_size,
+            args.image_vocab_size,
+            args.num_res_channels,
+            args.num_groups,
+        )
 
     def forward(self, input_ids, patches, positions, input_type, loss_mask, attention_mask) -> CausalLMOutputWithPast:
-        embeds = self.emb(input_ids, patches, positions, input_type, loss_mask, attention_mask)
-        labels = embeds["tokens"]
+        embeds = self.emb(input_ids, patches, positions, input_type, attention_mask)
+        labels = input_ids.clone()
         # All labels set to -100 are ignored (masked), the loss is only computed for labels in
         # [0, ..., config.vocab_size]
         labels[loss_mask == 0] = -100
--- tests/model/test_gia_model.py
+++ tests/model/test_gia_model.py
@@ -1,21 +1,48 @@
 import pytest
+import torch
 from accelerate import Accelerator
 from torch.utils.data import DataLoader
 
 from gia.config import Arguments
-from gia.datasets import load_gia_dataset
+from gia.datasets import load_gia_dataset, collate_fn
+from gia.datasets.utils import DatasetDict
 from gia.model import GiaModel
+from gia.processing import GiaProcessor
+
+
+def random_positions(size):  # Ensure that min < max
+    t1 = torch.rand(*size, 1, 2)  # Create a random tensor of shape (B, N, 1, 2) with values between 0 and 1
+    t2 = torch.rand(*size, 1, 2)  # Create another random tensor of shape (B, N, 1, 2) with values between 0 and 1
+    t_min = torch.min(t1, t2)  # Element-wise minimum of t1 and t2
+    t_max = torch.max(t1, t2)  # Element-wise maximum of t1 and t2
+    return torch.cat((t_min, t_max), dim=-2)  # Concatenate t_min and t_max along the proper dimension
 
 
 @pytest.mark.parametrize("use_accelerate", [True, False])
 def test_gia_accelerate(use_accelerate):
-    args = Arguments(task_names=["mujoco-ant", "metaworld-assembly"], embed_dim=48, seq_len=256)
+    args = Arguments(task_names=["mujoco-ant"], embed_dim=48, seq_len=256, output_dir="./")
     dataset = load_gia_dataset(args.task_names)
-    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+    processor = GiaProcessor(args)
+    dataset = DatasetDict(processor(**dataset))
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate_fn)
     model = GiaModel(args)
     if use_accelerate:
         accelerator = Accelerator()
         model, dataloader = accelerator.prepare(model, dataloader)
     batch = next(iter(dataloader))
-    output = model(batch["batch"])
+    output = model(**batch)
     assert output.loss.item() > 0.0
+    assert output.logits.shape == (args.batch_size, args.seq_len, 32000 + 1024 + 1)
+
+
+def test_model():
+    module = GiaModel(Arguments(output_dir="./"))
+    input_ids = torch.randint(0, 256, (2, 32))
+    patches = torch.rand(2, 32, 4, 16, 16)
+    positions = random_positions((2, 32))
+    input_type = torch.randint(0, 2, (2, 32))
+    loss_mask = torch.randint(0, 2, (2, 32), dtype=torch.bool)
+    attention_mask = torch.randint(0, 2, (2, 32), dtype=torch.bool)
+    output_tensor = module(input_ids, patches, positions, input_type, loss_mask, attention_mask)
+    assert output_tensor.loss.item() > 0.0
+    assert output_tensor.logits.shape == (2, 32, 32000 + 1024 + 1)
