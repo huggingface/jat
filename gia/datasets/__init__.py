c480bbb7ec1ae7763ab0ca404afe9b86f4126e46
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-05-17 15:56:51 +0200

    datasetdict in dataset.__init__

--- gia/datasets/__init__.py
+++ gia/datasets/__init__.py
@@ -1,3 +1,4 @@
 from .core import collate_fn, generate_prompts, load_gia_dataset
+from .utils import DatasetDict
 
-__all__ = ["collate_fn", "generate_prompts", "load_gia_dataset"]
+__all__ = ["collate_fn", "generate_prompts", "load_gia_dataset", "DatasetDict"]
