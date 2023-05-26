cd331fde507c74bc78e5e0fb45baa4d58b5b1b29
edbeeching <edbeeching@gmail.com>
2023-05-26 11:40:08 +0200

    increases the number of eps

--- data/envs/mujoco/create_mujoco_dataset.sh
+++ data/envs/mujoco/create_mujoco_dataset.sh
@@ -8,5 +8,5 @@ ENVS=(
 for ENV in "${ENVS[@]}"; do
     python -m sample_factory.huggingface.load_from_hub -r edbeeching/mujoco_${ENV}_1111 -d train_dir
     echo $ENV
-    python create_mujoco_dataset.py --env=mujoco_${ENV} --experiment=mujoco_${ENV}_1111 --train_dir=train_dir --push_to_hub --max_num_frames=1000000000 --no_render --max_num_episodes=100
+    python create_mujoco_dataset.py --env=mujoco_${ENV} --experiment=mujoco_${ENV}_1111 --train_dir=train_dir --push_to_hub --max_num_frames=1000000000 --no_render --max_num_episodes=10000
 done
