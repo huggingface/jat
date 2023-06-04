399b5fbff50d7e84c4850dbc2eb56fc365a8a2d9
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-06-04 23:07:04 +0200

    processing update

--- gia/processing/utils.py
+++ gia/processing/utils.py
@@ -1,28 +1,5 @@
-def collapse_none(x):
-    """
-    Recursively collapse list of None into None.
-
-    Args:
-        lst (Any): Any object. If it's a list, it will be recursively collapsed.
-
-    Example:
-        >>> collapse_none([1, 2])
-        [1, 2]
-        >>> collapse_none([1, 2, None])
-        [1, 2, None]
-        >>> collapse_none([1, 2, [None]])
-        [1, 2, None]
-        >>> collapse_none([1, 2, [None, None]])
-        [1, 2, None]
-        >>> collapse_none([1, 2, [None, 3]])
-        [1, 2, [None, 3]]
-        >>> collapse_none([1, 2, [None, [None]]])
-        [1, 2, None]
-        >>> collapse_none([None, None])
-        None
-    """
+def nested_like(x, val):
     if isinstance(x, list):
-        x = [collapse_none(xx) for xx in x]
-        if all(xx is None for xx in x):
-            return None
-    return x
+        return [nested_like(x_i, val) for x_i in x]
+    else:
+        return val
