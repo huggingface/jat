c6d269a622b9d778eb653ecb7a222efa346887aa
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2023-06-08 18:10:22 +0200

    update prediction example

--- examples/prediction.py
+++ examples/prediction.py
@@ -9,11 +9,10 @@ from gia.datasets import GIADataCollator, maybe_prompt_dataset
 from gia.model import GiaModel
 from gia.processing import GiaProcessor
 
-task_names = ["atari-alien", "mujoco-ant"]
+task_names = ["babyai-go-to", "mujoco-ant"]
 split = "train[:10]"
 
-args = Arguments(task_names=task_names, embed_dim=12, output_dir="./")
-processor = GiaProcessor(args)
+processor = GiaProcessor()
 
 # Load, prompt and process the dataset
 datasets = {task_name: load_dataset("gia-project/gia-dataset", task_name, split=split) for task_name in task_names}
@@ -32,9 +31,9 @@ dataset = Dataset.load_from_disk("./dataset")
 
 dataloader = DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=GIADataCollator())
 
-model = GiaModel(args)
+model = GiaModel(Arguments(embed_dim=12, output_dir="./"))
 
 for batch in tqdm(dataloader):
     with torch.no_grad():
-        print(batch.keys())
         output = model(**batch)
+        tqdm.write(f"Batch keys: {list(batch.keys())}  Loss: {output.loss.item():.2f}")
