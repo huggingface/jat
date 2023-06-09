fd1cc2dbb0395755448c21a523711c6c22e25cfe
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2023-06-09 16:31:55 +0200

    update dependency

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ REQUIRED_PKGS = [
     "hydra-core",
     "accelerate",
     "wandb",
-    "datasets @ git+https://github.com/qgallouedec/datasets.git@fix-IndexError-when-indexing-Sequence-of-Array2D-with-None-values",
+    "datasets @ git+https://github.com/huggingface/datasets.git@master",
     "opencv-python",
     "Pillow",
 ]
