d20217ef28d3a1e52c7732a7940bc1e91121aa7f
Edward Beeching <edbeeching@gmail.com>
2023-06-13 22:54:39 +0200

    adds gia_agent for rl eval

--- gia/eval/mujoco_evaluator.py
+++ gia/eval/mujoco_evaluator.py
@@ -1,15 +1,15 @@
 import gym
 import numpy as np
 import torch
-from datasets import load_dataset
+
 from tqdm import tqdm
 
 from gia import GiaConfig, GiaModel
 from gia.config.arguments import Arguments
-from gia.datasets import GiaDataCollator, Prompter
+
 from gia.eval.evaluator import Evaluator
 from gia.eval.mappings import DATASET_FILE_MAPPING, TASK_TO_ENV_MAPPING
-from gia.processing import GiaProcessor
+from gia.eval.rl.gia_agent import GiaAgent
 
 
 def make_mujoco_env(env_name, render_mode=None):
@@ -17,10 +17,10 @@ def make_mujoco_env(env_name, render_mode=None):
 
 
 class MujocoEvaluator(Evaluator):
-    def __init__(self, args: Arguments):
+    def __init__(self, args: Arguments, task_map=TASK_TO_ENV_MAPPING, dataset_map=DATASET_FILE_MAPPING):
         self.task = "mujoco"
-        self.env_names = TASK_TO_ENV_MAPPING[self.task]
-        self.data_filepaths = DATASET_FILE_MAPPING[self.task]
+        self.env_names = task_map[self.task]
+        self.data_filepaths = dataset_map[self.task]
         self.args = args
 
     def evaluate(self, model: GiaModel):
@@ -33,97 +33,20 @@ class MujocoEvaluator(Evaluator):
     def _evaluate_env(self, env_name: str, dataset_name: str, model: GiaModel):
         num_envs = 1
 
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         env = gym.vector.make(env_name, num_envs)
-        num_obs_tokens = env.observation_space.shape[1]
-        num_act_tokens = env.action_space.shape[1]
-        tokens_per_step = num_obs_tokens + num_act_tokens + int(self.args.use_separator)
-        int_per_seq = (self.args.seq_len // tokens_per_step) - 1
-        max_kv_size = int_per_seq * tokens_per_step
-
-        dataset = load_dataset("gia-project/gia-dataset", dataset_name, split="test")
-        prompter = Prompter(
-            dataset,
-            min_prompt_len=int_per_seq,
-            max_prompt_len=int_per_seq,
-        )
-
-        prompts = prompter.generate_prompts(self.args.n_episodes)
-
-        processor = GiaProcessor()
-        collator = GiaDataCollator()
-        token_shift = processor.tokenizer.token_shift
+        gia_agent = GiaAgent(self.args, dataset_name, model, env.observation_space, env.action_space)
 
         returns = []
         # due to how to KV cache is used, we only can evaluate one env instance at a time
         for ep in tqdm(range(self.args.n_episodes)):
-            prompt_observations = np.array([prompts["continuous_observations"][ep]])
-            prompt_actions = np.array([prompts["continuous_actions"][ep]])
-
-            processed_prompt = processor(
-                continuous_observations=prompt_observations,
-                continuous_actions=prompt_actions,
-                padding=False,
-                truncation="max_length",
-                truncation_side="left",
-                max_length=args.seq_len - num_act_tokens,  # ensure not to overflow when the actions are added
-            )
-
-            # TODO:
-            # - confirm attention masks are not needed in this setting
-
-            processed_prompt = collator([{key: processed_prompt[key][0] for key in processed_prompt.keys()}])
-            for key in processed_prompt.keys():
-                processed_prompt[key] = processed_prompt[key].to(device)
-            output = model(**processed_prompt, use_cache=True)
-            past_key_values = output.past_key_values
-
             accum_rewards = []
             done = False
             obs, info = env.reset()
+            gia_agent.reset()
 
             while not done:
                 # Compute the output of the model
-                processed = processor(
-                    continuous_observations=[obs],
-                    continuous_actions=[],
-                    padding=False,
-                    truncation="max_length",
-                    truncation_side="left",
-                    max_length=args.seq_len - num_act_tokens,  # ensure not to overflow when the actions are added
-                )
-                processed = collator([{key: processed[key][0] for key in processed.keys()}])
-                for key in processed.keys():
-                    processed[key] = processed[key].to(device)
-                action_tokens = []
-
-                for i in range(num_act_tokens):
-                    output = model(**processed, use_cache=True, past_key_values=past_key_values)
-                    past_key_values = output.past_key_values
-                    action_logits = output.logits[:, -1, token_shift:]
-
-                    action_token = torch.argmax(action_logits, -1) + token_shift
-                    action_tokens.append(action_token)
-
-                    processed["input_ids"] = action_token[None, :]
-                    if i == 0:  # only needs to be done once
-                        processed["loss_mask"] = torch.ones(1, 1, dtype=torch.bool, device=device)
-                        processed["input_types"] = torch.zeros(1, 1, dtype=torch.int64, device=device)
-                        processed["local_positions"] = -torch.ones(1, 1, dtype=torch.int64, device=device)
-
-                # to ensure the KV cache includes the last action token
-                output = model(**processed, use_cache=True, past_key_values=past_key_values)
-                past_key_values = output.past_key_values
-                if past_key_values[0][0].shape[2] > max_kv_size:
-                    # remove one step of tokens, to ensure context < 1024
-                    past_key_values = [
-                        (k[:, :, tokens_per_step:], v[:, :, tokens_per_step:]) for (k, v) in past_key_values
-                    ]
-                action_tokens = torch.stack(action_tokens, dim=-1)
-
-                # Decode the action tokens
-                action = processor.tokenizer.decode_continuous(action_tokens.cpu().numpy())
-                # TODO: Clamp action to be in domain of action space?
+                action = gia_agent.get_action(obs)
                 obs, reward, terminated, truncated, info = env.step(action)
 
                 done = terminated or truncated
@@ -139,8 +62,12 @@ if __name__ == "__main__":
 
     config = GiaConfig()
 
-    args = Arguments(output_dir="tmp", n_episodes=2)
-    model = GiaModel(config).to("cuda")
+    args = Arguments(output_dir="tmp", n_episodes=2, task_names="mujoco-doublependulum")
+    model = GiaModel(config)  # .to("cuda")
 
-    evaluator = MujocoEvaluator(args)
+    evaluator = MujocoEvaluator(
+        args,
+        task_map={"mujoco": ["InvertedDoublePendulum-v4"]},
+        dataset_map={"mujoco": ["mujoco-doublependulum"]},
+    )
     evaluator.evaluate(model)
---
+++ gia/eval/rl/gia_agent.py
@@ -0,0 +1,112 @@
+import torch
+import numpy as np
+
+from datasets import load_dataset
+
+from gia.config.arguments import Arguments
+from gia.datasets.core import Prompter
+from gia.model.gia_model import GiaModel
+from gia.processing import GiaProcessor
+from gia.datasets import GiaDataCollator, Prompter
+
+
+class GiaAgent:
+    def __init__(
+        self,
+        args: Arguments,
+        dataset_name: str,
+        model: GiaModel,
+        obs_space,
+        action_space,
+    ):
+        self.args = args
+        self.model = model
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        num_obs_tokens = obs_space.shape[1]
+        self.num_act_tokens = action_space.shape[1]
+        tokens_per_step = num_obs_tokens + self.num_act_tokens + int(self.args.use_separator)
+        int_per_seq = (self.args.seq_len // tokens_per_step) - 1
+
+        dataset = load_dataset("gia-project/gia-dataset", dataset_name, split="test")
+        prompter = Prompter(
+            dataset,
+            min_prompt_len=int_per_seq,
+            max_prompt_len=int_per_seq,
+        )
+
+        self.max_kv_size = int_per_seq * tokens_per_step
+        self.prompts = prompter.generate_prompts(self.args.n_episodes)
+
+        self.processor = GiaProcessor()
+        self.collator = GiaDataCollator()
+        self.token_shift = self.processor.tokenizer.token_shift
+        self._ep_index = -1
+
+    def reset(self) -> None:
+        self._ep_index += 1
+        assert self._ep_index < self.args.n_episodes
+
+        prompt_observations = np.array([self.prompts["continuous_observations"][self._ep_index]])
+        prompt_actions = np.array([self.prompts["continuous_actions"][self._ep_index]])
+
+        processed_prompt = self.processor(
+            continuous_observations=prompt_observations,
+            continuous_actions=prompt_actions,
+            padding=False,
+            truncation="max_length",
+            truncation_side="left",
+            max_length=self.args.seq_len - self.num_act_tokens,  # ensure not to overflow when the actions are added
+        )
+
+        # TODO:
+        # - confirm attention masks are not needed in this setting
+
+        processed_prompt = self.collator([{key: processed_prompt[key][0] for key in processed_prompt.keys()}])
+        for key in processed_prompt.keys():
+            processed_prompt[key] = processed_prompt[key].to(self.device)
+        output = self.model(**processed_prompt, use_cache=True)
+        self._past_key_values = output.past_key_values
+
+    def get_action(self, obs) -> np.array:
+        processed = self.processor(
+            continuous_observations=[obs],
+            continuous_actions=[],
+            padding=False,
+            truncation="max_length",
+            truncation_side="left",
+            max_length=self.args.seq_len - self.num_act_tokens,  # ensure not to overflow when the actions are added
+        )
+        processed = self.collator([{key: processed[key][0] for key in processed.keys()}])
+        for key in processed.keys():
+            processed[key] = processed[key].to(self.device)
+        action_tokens = []
+
+        for i in range(self.num_act_tokens):
+            output = self.model(**processed, use_cache=True, past_key_values=self._past_key_values)
+            self._past_key_values = output.past_key_values
+            action_logits = output.logits[:, -1, self.token_shift :]
+
+            action_token = torch.argmax(action_logits, -1) + self.token_shift
+            action_tokens.append(action_token)
+
+            processed["input_ids"] = action_token[None, :]
+            if i == 0:  # only needs to be done once
+                processed["loss_mask"] = torch.ones(1, 1, dtype=torch.bool, device=self.device)
+                processed["input_types"] = torch.zeros(1, 1, dtype=torch.int64, device=self.device)
+                processed["local_positions"] = -torch.ones(1, 1, dtype=torch.int64, device=self.device)
+
+        # to ensure the KV cache includes the last action token
+        output = self.model(**processed, use_cache=True, past_key_values=self._past_key_values)
+        self._past_key_values = output.past_key_values
+        if self.past_key_values[0][0].shape[2] > self.max_kv_size:
+            # remove one step of tokens, to ensure context < 1024
+            self._past_key_values = [
+                (k[:, :, self.tokens_per_step :], v[:, :, self.tokens_per_step :]) for (k, v) in self._past_key_values
+            ]
+        action_tokens = torch.stack(action_tokens, dim=-1)
+
+        # Decode the action tokens
+        action = self.processor.tokenizer.decode_continuous(action_tokens.cpu().numpy())
+        # TODO: Clamp action to be in domain of action space?
+        return action
