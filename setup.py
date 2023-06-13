eb265ca6ae28a7f344818678ff38e80130e982f3
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-06-13 14:53:38 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ REQUIRED_PKGS = [
     "hydra-core",
     "accelerate",
     "wandb",
-    "datasets @ git+https://github.com/huggingface/datasets.git@main",
+    "datasets @ git+https://github.com/qgallouedec/datasets.git@5936-sequence-of-array-not-supported-for-most-dtype",
     "opencv-python",
     "Pillow",
 ]
