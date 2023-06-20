93da43dd6e2e33dd08ce788500368c9d5eebffc1
Edward Beeching <edbeeching@gmail.com>
2023-06-20 13:39:41 +0200

    merged all language modeling evaluators into one generic evaluator.

--- gia/eval/language_modeling/conceptual_captions_evaluator.py
+++
@@ -1,9 +0,0 @@
-from gia.config.arguments import Arguments
-
-from .base_language_modeling_evaluator import BaseLanguageModelingEvaluator
-
-
-class ConceptualCaptionsEvaluator(BaseLanguageModelingEvaluator):
-    def __init__(self, args: Arguments):
-        self.task = "conceptual-captions"
-        super().__init__(args)
