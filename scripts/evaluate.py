b6b76e7781be94e6301946a14130120d6de2b0ec
edbeeching <edbeeching@gmail.com>
2023-06-21 13:20:46 +0200

    fixes a few bugs in evaluate script

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -5,27 +5,26 @@ import torch
 import wandb
 from gia import GiaConfig, GiaModel
 from gia.config import Arguments
-from gia.eval.utils import EVALUATORS, get_domain
+from gia.eval.utils import get_evaluator
 
 
 def main():
     args = Arguments.parse_args()
     # connect to (existing) wandb
-    wandb.init()
+    wandb.init(resume="must", id=args.wandb_run_id, group=args.wandb_run_group, project=args.wandb_project)
     wandb.define_metric("eval/step")
     wandb.define_metric("eval/*", step_metric="eval/step")
 
     config = GiaConfig.from_args(args)
     model = GiaModel(config).to("cuda")
 
-    for checkpoint_path in args.eval_checkpoints:
-        step = int(checkpoint_path.split("-")[-1])
-        state_dict = torch.load(os.path.join(checkpoint_path, "pytorch_model.bin"))
+    for checkpoint in args.eval_checkpoints:
+        step = int(checkpoint.split("-")[-1])
+        state_dict = torch.load(os.path.join(args.output_dir, checkpoint, "pytorch_model.bin"))
         model.load_state_dict(state_dict)
         model.eval()
         for task in args.task_names:
-            domain = get_domain(task)
-            evaluator_cls = EVALUATORS[domain]
+            evaluator_cls = get_evaluator(task)
             evaluator = evaluator_cls(args, task)
             result = evaluator.evaluate(model)
 
