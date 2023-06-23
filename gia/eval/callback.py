a0e73ab1b4c31fa0d0addf963ee38bc4604e9b74
edbeeching <edbeeching@gmail.com>
2023-06-23 11:56:19 +0200

    wandb init fix

--- gia/eval/callback.py
+++ gia/eval/callback.py
@@ -19,6 +19,12 @@ class EvaluateCheckpointCallback(TrainerCallback):
     def __init__(self) -> None:
         self._logged_files = set()
 
+    def on_init_end(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
+        wandb.init(id=args.wandb_run_id, group=args.wandb_run_group, project=args.wandb_project)
+        # for custom x-axis on evals
+        wandb.define_metric("eval/step")
+        wandb.define_metric("eval/*", step_metric="eval/step")
+
     def on_save(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
         if not Accelerator().is_main_process:
             # otherwise multi-GPU jobs will launch several evals.
@@ -54,9 +60,6 @@ class EvaluateCheckpointCallback(TrainerCallback):
         if not args.wandb_enabled:
             return
 
-        # for custom x-axis on evals
-        wandb.define_metric("eval/step")
-        wandb.define_metric("eval/*", step_metric="eval/step")
         try:  # I don't want this stopping training due to issues loading files etc
             all_eval_jsons = glob.glob(f"{args.output_dir}/eval/**/*.json", recursive=True)
             to_log = self._logged_files.difference(all_eval_jsons)
