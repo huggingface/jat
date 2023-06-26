08ef895faff731fdc3c563c2d4eb13f742d1591d
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2023-06-26 19:51:48 +0200

    remove uncertainties

--- gia/utils/utils.py
+++ gia/utils/utils.py
@@ -1,18 +1,4 @@
 from transformers.utils import logging
-from uncertainties import ufloat
-from uncertainties.core import Variable
-
-
-def ufloat_encoder(obj):
-    if isinstance(obj, Variable):
-        return {"__ufloat__": True, "n": obj.n, "s": obj.s}
-    return obj
-
-
-def ufloat_decoder(dct):
-    if "__ufloat__" in dct:
-        return ufloat(dct["n"], dct["s"])
-    return dct
 
 
 logger = logging.get_logger("gia")
