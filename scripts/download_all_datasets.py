9c32645d1b6d809221873622f24b0a9cd02c6328
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2023-07-03 13:39:31 +0200

    update download_all_dataset

--- scripts/download_all_datasets.py
+++ scripts/download_all_datasets.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 """Load and generate batch for all datasets from the GIA dataset"""
 
-from datasets import get_dataset_config_names
+from datasets import get_dataset_config_names, load_dataset
 
-from gia.config import DatasetArguments
-from gia.datasets import load_batched_dataset
-
-
-args = DatasetArguments()
 
 task_names = get_dataset_config_names("gia-project/gia-dataset")  # get all task names from gia dataset
 for task_name in task_names:
     print(f"Loading {task_name}...")
-    load_batched_dataset(task_name, args)
+    load_dataset("gia-project/gia-dataset", task_name)
