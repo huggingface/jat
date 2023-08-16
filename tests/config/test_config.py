e73867c5360684698fea1da2ff66bf23b1679fbb
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-08-16 12:53:36 +0200

    test loading processor from pretrained

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -2,6 +2,8 @@ import pytest
 from transformers import AutoConfig, AutoModel
 
 import gia  # noqa
+from gia import GiaConfig, GiaModel
+from gia.processing import GiaProcessor
 
 
 @pytest.mark.parametrize("model_size", ["80m", "387m", "1.27b"])
@@ -13,3 +15,10 @@ def test_auto_config(model_size):
 def test_auto_model(model_size):
     config = AutoConfig.from_pretrained(f"gia-project/gia-{model_size}")
     AutoModel.from_config(config)
+
+
+@pytest.mark.parametrize("model_size", ["80m", "387m", "1.27b"])
+def test_load_model_and_processor(model_size):
+    config = GiaConfig.from_pretrained(f"gia-project/gia-{model_size}")
+    GiaProcessor.from_pretrained(f"gia-project/gia-{model_size}")
+    GiaModel(config)
