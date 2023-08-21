d56a97476fc2b6a62932ac521fe6261927aa9df6
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2023-08-21 09:31:41 +0200

    parallel generation script for conceptual captions

--- data/conceptual_captions/generate_conceptual_caption.py
+++ data/conceptual_captions/generate_conceptual_caption.py
@@ -1,37 +1,37 @@
+import concurrent.futures
 import io
+import os
 import urllib
-from concurrent.futures import ThreadPoolExecutor
-from functools import partial
 
-import numpy as np
 import PIL.Image
 from datasets import load_dataset
 from datasets.utils.file_utils import get_datasets_user_agent
 
-
 USER_AGENT = get_datasets_user_agent()
+PATH = "data/test"  # or "data/train"
+
+MAX_WORKERS = 10  # adjust to your needs
+MAX_QUEUE_SIZE = 2 * MAX_WORKERS  # adjust to your needs
 
 
-def fetch_single_image(image_url, timeout=1, retries=0):
-    for _ in range(retries + 1):
-        try:
-            request = urllib.request.Request(
-                image_url,
-                data=None,
-                headers={"user-agent": USER_AGENT},
-            )
-            with urllib.request.urlopen(request, timeout=timeout) as req:
-                image = PIL.Image.open(io.BytesIO(req.read()))
-            break
-        except Exception:
-            image = None
+def fetch_single_image(image_url, timeout=1):
+    print(image_url)
+    try:
+        request = urllib.request.Request(
+            image_url,
+            data=None,
+            headers={"user-agent": USER_AGENT},
+        )
+        with urllib.request.urlopen(request, timeout=timeout) as req:
+            image = PIL.Image.open(io.BytesIO(req.read()))
+    except Exception:
+        image = None
     return image
 
 
 def resize_single_image(image: PIL.Image):
     # Resize so that the bigger size is at most 352
     width, height = image.size
-    print("Image size", image.size, "patches", f"{width // 16}x{height // 16} = {width // 16*height // 16}")
     if width > height:
         new_width = 352
         new_height = int(height * 352 / width)
@@ -39,45 +39,43 @@ def resize_single_image(image: PIL.Image):
         new_height = 352
         new_width = int(width * 352 / height)
     image = image.resize((new_width, new_height), PIL.Image.BILINEAR)
-    image = np.array(image, dtype=np.uint8)
+    image = image.convert("RGB")
     return image
 
 
-def fetch_images(batch, timeout=1, retries=0):
-    fetch_single_image_with_args = partial(fetch_single_image, timeout=timeout, retries=retries)
-    with ThreadPoolExecutor() as executor:
-        batch["image"] = list(executor.map(fetch_single_image_with_args, batch["image_url"]))
-    return batch
+dataset = load_dataset("conceptual_captions", split="validation")  # or "train"
+if not os.path.exists(f"{PATH}/metadata.csv"):
+    with open(f"{PATH}/metadata.csv", "w") as f:
+        f.write("file_name,caption,idx\n")
+    dataset_idx = 0
+    image_idx = 0
+else:  # get the lastest index
+    with open(f"{PATH}/metadata.csv", "r") as f:
+        lines = f.readlines()
+        image_idx = len(lines) - 1
+        dataset_idx = int(lines[-1].split(",")[-1]) + 1
+        print(image_idx, dataset_idx)
 
+with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+    future_to_idx = {executor.submit(fetch_single_image, dataset[dataset_idx]["image_url"]): dataset_idx}
+    dataset_idx += 1
+    while dataset_idx < len(dataset):
+        done, _ = concurrent.futures.wait(future_to_idx, return_when=concurrent.futures.FIRST_COMPLETED)
+        for future in done:
+            idx = future_to_idx.pop(future)
+            try:
+                image = future.result()
+                if image is not None:
+                    image = resize_single_image(image)
+                    sample = dataset[idx]
+                    caption = sample["caption"].replace(",", "").replace(";", "").replace("\n", "").replace("\t", "")
+                    image.save(f"{PATH}/{image_idx:07d}.png", "PNG")
+                    with open(f"{PATH}/metadata.csv", "a") as f:
+                        f.write(f"{image_idx:07d}.png,{caption},{idx}\n")
+                    image_idx += 1
+            except Exception as exc:
+                print(f"Generated an exception: {exc}")
 
-dataset = load_dataset("conceptual_captions", split="validation")
-data_path = "data/textual/conceptual_captions/data"
-with open(f"{data_path}/test/metadata.csv", "w") as f:
-    f.write("file_name,additional_feature\n")
-
-idx = 1
-for sample in dataset:
-    url = sample["image_url"]
-
-    # print(url)
-    image = fetch_single_image(url)
-    if image is None:
-        print("Failed to fetch image")
-        continue
-    image = resize_single_image(image)
-    caption = sample["caption"]
-    image = PIL.Image.fromarray(image)
-    # Save image under the name 0001.png, 002.png, etc.
-    image.save(f"{data_path}/test/{idx:06d}.png")
-
-    # Add a line to the metadata.csv file
-    # 0003.png,This is the caption of the third image
-    # etc.
-    # Also make sure to remove "," from the captions.
-
-    with open(f"{data_path}/test/metadata.csv", "a") as f:
-        # remove , from the caption
-        caption = caption.replace(",", "")
-        f.write(f"{idx:04d}.png,{caption}\n")
-
-    idx += 1
+        while len(future_to_idx) < MAX_QUEUE_SIZE and dataset_idx < len(dataset):
+            future_to_idx[executor.submit(fetch_single_image, dataset[dataset_idx]["image_url"])] = dataset_idx
+            dataset_idx += 1
