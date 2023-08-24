e7fe861a3c3d0a266c09ee559058ad3499bffe37
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-08-24 18:11:22 +0200

    fix test

--- tests/eval/rl/test_gia_agent.py
+++ tests/eval/rl/test_gia_agent.py
@@ -1,11 +1,12 @@
+import gymnasium as gym
 import pytest
+import torch
 
 from gia import GiaConfig, GiaModel
+from gia.eval.rl import make
 from gia.eval.rl.gia_agent import GiaAgent
 from gia.processing import GiaProcessor
-from gia.eval.rl import make
-import gymnasium as gym
-import torch
+
 
 @pytest.mark.parametrize("task_name", ["atari-alien", "babyai-action-obj-door", "metaworld-assembly", "mujoco-ant"])
 def test_gia_agent(task_name):
@@ -21,25 +22,12 @@ def test_gia_agent(task_name):
     agent = GiaAgent(model, processor, task_name, num_envs, use_prompt=False)
 
     # Initialize the environment and the agent
-    obs, info = vec_env.reset()
+    obs, _ = vec_env.reset()
     agent.reset()
 
-    # Run the agent for 100 steps
-    all_episode_rewards = []
-    for t in range(1_000):
+    # Run the agent for 1k steps
+    for _ in range(1_000):
         with torch.inference_mode():
             action = agent.get_action(obs)
-        obs, reward, truncated, terminated, info = vec_env.step(action)
-        if terminated or truncated:
-            obs, info = vec_env.reset()
-            agent.reset()
-            print(episode_reward)
-            all_episode_rewards.append(episode_reward)
-            episode_reward = 0
+        obs, _, _, _, _ = vec_env.step(action)
     vec_env.close()
-
-
-    observations = np.array([agent.observation_space.sample() for _ in range(num_envs)])
-    actions = agent.get_action(observations)
-    for action in actions:
-        assert agent.action_space.contains(action)
