217f1c9fefc909c52b4a6c1658240c683de9be22
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2023-08-24 18:32:09 +0200

    revert surrond-only

--- data/envs/atari/generate_random_score.py
+++ data/envs/atari/generate_random_score.py
@@ -129,6 +129,5 @@ def generate_random_score(task_name):
 
 if __name__ == "__main__":
     register_atari_components()
-    generate_random_score("atari-surround")
-    # with Pool(32) as p:
-    #     p.map(generate_random_score, TASK_NAMES)
+    with Pool(32) as p:
+        p.map(generate_random_score, TASK_NAMES)
