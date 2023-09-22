a0a7e3d1423c361e81f7e57ab452bc2660b68a98
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2023-09-22 20:33:30 +0200

    fix __init__

--- gia2/__init__.py
+++ gia2/__init__.py
@@ -1,5 +1,5 @@
-from .config import Gia2Config
-from .modeling import Gia2Model
+from .configuration_gia2 import Gia2Config
+from .modeling_gia2 import Gia2Model
 
 
 __all__ = ["Gia2Model", "Gia2Config"]
