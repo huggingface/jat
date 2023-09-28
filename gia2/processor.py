e601789330e8f73891f165986bd9a2ca1f91fa53
cromac <clement.romac@gmail.com>
2023-09-28 16:24:43 +0200

    Adding other modalities (to test)

--- gia2/processor.py
+++ gia2/processor.py
@@ -26,6 +26,20 @@ class GIAProcessor(GitProcessor):
             encoding = self._cut_text(encoded_text, max_input_size)  # truncate and break down into multiple sampls if necessary
         elif "text" in examples and "images" in examples:
             encoding = super().__call__(examples["text"], examples["images"], return_tensors, **kwargs)
+        else:
+            max_len = max_input_size // 2
+            encoding = {key: [] for key in examples.keys()}
+            first_ep_batch = next(iter(examples.values()))
+            num_episodes = len(first_ep_batch)
+            for ep_idx in range(num_episodes):
+                ep_len = len(first_ep_batch[ep_idx])
+                for t in range(0, ep_len, max_len):
+                    for key in examples.keys():
+                        if hasattr(examples[key][ep_idx], "__len__"):
+                            chunk = examples[key][ep_idx][t: t + max_len]
+                        else:
+                            chunk = examples[key][ep_idx]
+                        encoding[key].append(chunk)
 
         return encoding
 
