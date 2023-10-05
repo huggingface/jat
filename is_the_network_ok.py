2d3615e910b7389d1cd7d10f5ee92153a8419c62
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2023-10-05 10:15:40 +0200

    finish

--- is_the_network_ok.py
+++ is_the_network_ok.py
@@ -1,44 +1,91 @@
+import torch
+import torch.optim as optim
+from datasets import load_dataset
 from torch import nn
+from torch.nn import CrossEntropyLoss
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 
 class MyEncoder(nn.Module):
-    def __init__(self, input_size, embedding_dim) -> None:
+    def __init__(self, input_size, num_logits, embedding_dim, hidden_size) -> None:
         super().__init__()
-        self.embedding = nn.Embedding(num_embeddings=148, embedding_dim=embedding_dim)
-        self.linear = nn.Linear(in_features=input_size * embedding_dim)
+        self.embedding = nn.Embedding(num_embeddings=num_logits, embedding_dim=embedding_dim)
+        self.linear = nn.Linear(in_features=input_size * embedding_dim, out_features=hidden_size)
 
     def forward(self, x):
-        x = self.embedding(x)
-        x = x.flatten(-1)
-        x = self.linear(x)
+        x = self.embedding(x)  # B x S x E
+        x = torch.flatten(x, start_dim=-2)  # B x (S*E)
+        x = self.linear(x)  # B x H
         return x
-    
+
+
 class MyDecoder(nn.Module):
-    def __init__(self, output_size, embedding_dim) -> None:
+    def __init__(self, hidden_size, embedding_dim, input_size, num_logits) -> None:
         super().__init__()
+        self.input_size = input_size
         self.embedding_dim = embedding_dim
-        # Assuming that the encoder's linear layer output size is `output_size`
-        self.linear = nn.Linear(in_features=output_size, out_features=embedding_dim*30)
-        # Additional layer to reconstruct the original input
-        self.output_layer = nn.Linear(in_features=embedding_dim*30, out_features=output_size)
-        
+        self.linear = nn.Linear(in_features=hidden_size, out_features=embedding_dim * input_size)
+        self.output_layer = nn.Linear(in_features=embedding_dim, out_features=num_logits)
+
     def forward(self, x):
-        x = self.linear(x)
-        # Reshape the output to [batch_size, seq_length, embedding_dim]
-        x = x.view(-1, 148, self.embedding_dim)
-        # You might apply some operation to get back to the original input
-        # For example, if the original input was one-hot encoded, you might apply a softmax operation
-        x = self.output_layer(x)
+        x = self.linear(x)  # B x (S*E)
+        x = torch.unflatten(x, dim=-1, sizes=(self.input_size, self.embedding_dim))  # B x S x E
+        x = self.output_layer(x)  # B x S x num_logits
         return x
 
 
-from datasets import load_dataset
 dataset = load_dataset("gia-project/gia-dataset-parquet", "babyai-go-to", split="train")
+
 
 def func(examples):
     flat = [obs for ep in examples["discrete_observations"] for obs in ep]
     return {"discrete_observations": flat}
 
+
 dataset = dataset.map(func, batched=True, remove_columns=["text_observations", "rewards", "discrete_actions"])
 
 print(len(dataset))
+num_logits = 10
+input_size = len(dataset[0]["discrete_observations"])  # num integers in the input
+embedding_dim = 5  # embedding dimension
+hidden_size = 768  # hidden size of the encoder's linear layer
+
+encoder = MyEncoder(input_size, num_logits, embedding_dim, hidden_size).to("cuda")
+decoder = MyDecoder(hidden_size, embedding_dim, input_size, num_logits).to("cuda")
+
+# Hyperparameters
+learning_rate = 0.001
+num_epochs = 10
+batch_size = 64
+
+
+def collate(batch):
+    return torch.tensor([x["discrete_observations"] for x in batch]).to("cuda")
+
+
+# Create DataLoader
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=collate)
+
+# Define loss function and optimizer
+criterion = CrossEntropyLoss()
+optimizer = optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), lr=learning_rate)
+
+# Training loop
+for epoch in range(num_epochs):
+    for idx, batch in tqdm(enumerate(dataloader)):
+        encoder_output = encoder(batch)
+        decoder_output = decoder(encoder_output)
+        loss = criterion(decoder_output.view(-1, num_logits), batch.view(-1))
+
+        accuracy = (decoder_output.argmax(dim=-1) == batch).float().mean()
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if idx % 100 == 0:
+            print(
+                f"Epoch [{epoch+1}/{num_epochs}], Step [{idx+1}/{len(dataloader)}], "
+                f"Loss: {loss.item():.4f}, Accuracy: {accuracy:.4f}"
+            )
