05cba03cc84f768bc8add45739d805877a2fff98
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2023-10-05 17:38:08 +0200

    save to disk in download script

--- scripts/download_all_datasets.py
+++ scripts/download_all_datasets.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 """Load and generate batch for all datasets from the GIA dataset"""
 
+import argparse
+
 from datasets import get_dataset_config_names, load_dataset
+from datasets.config import HF_DATASETS_CACHE
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--tasks", nargs="+", default=[])
 
-task_names = get_dataset_config_names("gia-project/gia-dataset-parquet")  # get all task names from gia dataset
-for task_name in task_names:
-    print(f"Loading {task_name}...")
-    load_dataset("gia-project/gia-dataset-parquet", task_name)
+tasks = parser.parse_args().tasks
+if tasks == ["all"]:
+    tasks = get_dataset_config_names("gia-project/gia-dataset-parquet")  # get all task names from gia dataset
+
+for task in tasks:
+    print(f"Loading {task}...")
+    dataset = load_dataset("gia-project/gia-dataset-parquet", task)
+    dataset.save_to_disk(f"{HF_DATASETS_CACHE}/gia-project/gia-dataset-parquet/{task}")
