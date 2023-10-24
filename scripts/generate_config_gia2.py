781fae035d12f04f3b586e107d7051c8011f2c4c
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2023-10-24 13:52:46 +0200

    fix vocab size in config

--- scripts/generate_config_gia2.py
+++ scripts/generate_config_gia2.py
@@ -5,7 +5,9 @@ from gia2.processing_gia2 import Gia2Processor
 
 
 # Small model
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", model_input_names=["input_ids", "attention_mask"])
 config = Gia2Config(
+    vocab_size=tokenizer.vocab_size,
     max_position_embeddings=512,
     hidden_size=768,
     num_layers=12,
@@ -16,14 +18,15 @@ config = Gia2Config(
 image_processor = CLIPImageProcessor(
     size={"shortest_edge": config.image_size}, crop_size={"height": config.image_size, "width": config.image_size}
 )
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", model_input_names=["input_ids", "attention_mask"])
 tokenizer.model_max_length = config.max_position_embeddings
 processor = Gia2Processor(tokenizer=tokenizer, image_processor=image_processor)
 config.push_to_hub("gia-project/gia2-small")
 processor.push_to_hub("gia-project/gia2-small")
 
 # Medium model
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", model_input_names=["input_ids", "attention_mask"])
 config = Gia2Config(
+    vocab_size=tokenizer.vocab_size,
     max_position_embeddings=1024,
     hidden_size=2048,
     num_layers=24,
@@ -34,14 +37,15 @@ config = Gia2Config(
 image_processor = CLIPImageProcessor(
     size={"shortest_edge": config.image_size}, crop_size={"height": config.image_size, "width": config.image_size}
 )
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", model_input_names=["input_ids", "attention_mask"])
 tokenizer.model_max_length = config.max_position_embeddings
 processor = Gia2Processor(tokenizer=tokenizer, image_processor=image_processor)
 config.push_to_hub("gia-project/gia2-medium")
 processor.push_to_hub("gia-project/gia2-medium")
 
 # Large model
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", model_input_names=["input_ids", "attention_mask"])
 config = Gia2Config(
+    vocab_size=tokenizer.vocab_size,
     max_position_embeddings=2048,
     hidden_size=2560,
     num_layers=32,
@@ -52,7 +56,6 @@ config = Gia2Config(
 image_processor = CLIPImageProcessor(
     size={"shortest_edge": config.image_size}, crop_size={"height": config.image_size, "width": config.image_size}
 )
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", model_input_names=["input_ids", "attention_mask"])
 tokenizer.model_max_length = config.max_position_embeddings
 processor = Gia2Processor(tokenizer=tokenizer, image_processor=image_processor)
 config.push_to_hub("gia-project/gia2-large")
